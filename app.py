e1f6e29bc1e7380b22ab2ce30947f26c10413f9a
franciscof <franciscof@cetsp.com.br>
2024-10-15 00:05:08 -0300

    Projeto inicial

---
+++ app.py
@@ -0,0 +1,8 @@
+print('Hello World\n')
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu a opção', opcao_escolhida)
