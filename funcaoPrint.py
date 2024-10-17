1357ec92402c533fd55ba1d5852df51f9ed8ecf8
franciscof <franciscof@cetsp.com.br>
2024-10-17 00:04:08 -0300

    Conclui o estudo da função print()

---
+++ funcaoPrint.py
@@ -0,0 +1,14 @@
+#Execício 1
+print('Python na Escola de Programação da Alura.')
+
+# Exercício 2
+nome = 'Francisco'
+idade = 53
+print(f'Meu nome é {nome} e tenho {idade} anos.')
+
+# Exercício 3
+print('A', 'L', 'U', 'R', 'A', sep='\n')
+
+# Exercíco 4
+pi = 3.14159
+print(f'O valor arredondado de pi é: {pi:.2f}')
